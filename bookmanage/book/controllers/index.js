af57d545b8e848bb3d32923b08736b574b0ff5f7
Rbin-life <wspring542568096@gmail.com>
2016-01-10 00:02:40 +0800

    a

--- bookmanage/book/controllers/index.js
+++
@@ -1,7 +0,0 @@
-var Controller={
-	user:require("./user"),
-	manage:require("./manage"),
-	publish:require("./publish")
-};
-
-module.exports=Controller;
--- bookmanage/book/controllers/tool.js
+++
@@ -1,8 +0,0 @@
-var moment = require('moment');
-
-moment.locale("zh-cn");
-
-exports.formatDate=function(date){
-	date=moment(date);
-	return date.format('YYYY-MM-DD');
-}
